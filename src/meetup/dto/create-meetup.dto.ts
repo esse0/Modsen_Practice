424ac4f47ca66a8021c7f8cc5f8325fd296044c0
esse <redstrixw@gmail.com>
2024-06-19 14:18:54 +0300

    added meetup create dto

--- src/meetup/dto/create-meetup.dto.ts
+++ src/meetup/dto/create-meetup.dto.ts
@@ -1,1 +1,26 @@
-export class CreateMeetupDto {}
+import { IsNotEmpty, IsString, IsArray, ArrayMinSize, IsDateString, NotContains } from "class-validator";
+
+export class CreateMeetupDto {
+    @IsNotEmpty()
+    @IsString()
+    topic: String;
+
+    @IsNotEmpty()
+    @IsString()
+    description: String;
+
+    @IsNotEmpty()
+    @IsArray()
+    @IsString({ each: true })
+    @NotContains(" ", {each: true})
+    @ArrayMinSize(0)
+    tags: String[];
+
+    @IsNotEmpty()
+    @IsDateString()
+    eventDateTime: Date;
+
+    @IsNotEmpty()
+    @IsString()
+    address: String;
+}
