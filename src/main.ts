1f30ae7895b5a9abc0779ad87b2ecffc11c8379e
esse <redstrixw@gmail.com>
2024-06-19 14:19:22 +0300

    added global validation pipes

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 3000;
@@ -7,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe({whitelist: true}));
 
   await app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
 }
