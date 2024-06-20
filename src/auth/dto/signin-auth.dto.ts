6678a92f4493386be4de8631b5fc7314f8e1bf4f
esse <redstrixw@gmail.com>
2024-06-20 13:03:18 +0300

    created auth dto

--- src/auth/dto/signin-auth.dto.ts
+++ src/auth/dto/signin-auth.dto.ts
@@ -1,1 +1,16 @@
-export class SignInAuthDto {}
+import { IsEmail, IsNotEmpty, IsString, Length, Matches, NotContains } from "class-validator";
+
+export class SignInAuthDto {
+    @IsNotEmpty()
+    @IsEmail()
+    @NotContains(" ")
+    email: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @NotContains(" ")
+    @Length(5, 20, { message: 'Password has to be at between 5 and 20 chars'})
+    @Matches(/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#$%&? "])[a-zA-Z0-9!#$%&?]{0,25}$/, 
+    {message: 'The password should contain at least 1 uppercase character, 1 lowercase, 1 number'})
+    password: string;
+}
--- src/auth/dto/signup-auth.dto.ts
+++ src/auth/dto/signup-auth.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { SignInAuthDto } from './signin-auth.dto'
+import { IsEnum, IsNotEmpty, NotContains } from 'class-validator';
+import { Roles } from 'src/common/roles.enum';
 
-export class SignUpAuthDto extends PartialType(SignInAuthDto) {}
+export class SignUpAuthDto extends PartialType(SignInAuthDto) {
+    @NotContains(" ")
+    @IsNotEmpty()
+    @IsEnum(Roles)
+    role: string;
+}
