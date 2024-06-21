5d93a4c923cc46116adbcd91aa2dadeac830b08f
esse <redstrixw@gmail.com>
2024-06-21 11:03:45 +0300

    fixed meetup controller

--- src/meetup/meetup.controller.ts
+++ src/meetup/meetup.controller.ts
@@ -1,39 +1,51 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, UseGuards, ParseIntPipe } from '@nestjs/common';
 import { MeetupService } from './meetup.service';
 import { CreateMeetupDto } from './dto/create-meetup.dto';
 import { UpdateMeetupDto } from './dto/update-meetup.dto';
+import { GetCurrentUser } from 'src/common/decorators';
+import { AtGuard } from 'src/common/guards';
+import { Roles } from 'src/common/decorators/RoleDecorator';
+import { RoleGuard } from 'src/common/guards/role.guard';
 
 @Controller('meetup')
 export class MeetupController {
   constructor(private readonly meetupService: MeetupService) {}
 
+  @UseGuards(AtGuard, RoleGuard)
+  @Roles("ADMIN")
   @HttpCode(201)
-  @Post()
-  async create(@Body() createMeetupDto: CreateMeetupDto) {
-    return this.meetupService.create(createMeetupDto);
+  @Post('create')
+  async create(@GetCurrentUser('id') userId, @Body() createMeetupDto: CreateMeetupDto) {
+    return this.meetupService.create(createMeetupDto, userId);
   }
 
+  @UseGuards(AtGuard)
   @HttpCode(200)
-  @Get()
+  @Get('getall')
   async findAll() {
     return this.meetupService.findAll();
   }
 
+  @UseGuards(AtGuard)
   @HttpCode(200)
   @Get(':id')
-  async findOne(@Param('id') id: string) {
-    return this.meetupService.findOne(+id);
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.meetupService.findOne(id);
   }
 
+  @UseGuards(AtGuard, RoleGuard)
+  @Roles("ADMIN")
   @HttpCode(200)
   @Patch(':id')
-  async update(@Param('id') id: string, @Body() updateMeetupDto: UpdateMeetupDto) {
-    return this.meetupService.update(+id, updateMeetupDto);
+  async update(@GetCurrentUser('id') userId, @Param('id', ParseIntPipe) id: number, @Body() updateMeetupDto: UpdateMeetupDto) {
+    return this.meetupService.update(id, userId, updateMeetupDto);
   }
 
+  @UseGuards(AtGuard, RoleGuard)
+  @Roles("ADMIN")
   @HttpCode(200)
   @Delete(':id')
-  async remove(@Param('id') id: string) {
-    return this.meetupService.remove(+id);
+  async remove(@GetCurrentUser('id') userId, @Param('id', ParseIntPipe) id: number) {
+    return this.meetupService.remove(id, userId);
   }
 }
