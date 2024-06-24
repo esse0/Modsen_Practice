53254c2c97bf402def541bbdaa791f9242483006
esse <redstrixw@gmail.com>
2024-06-24 15:15:38 +0300

    fixed signup dto

--- src/auth/dto/signup-auth.dto.ts
+++ src/auth/dto/signup-auth.dto.ts
@@ -1,10 +1,23 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { SignInAuthDto } from './signin-auth.dto'
-import { IsEnum, IsNotEmpty, NotContains } from 'class-validator';
+import { IsEmail, IsEnum, IsNotEmpty, IsString, Length, Matches, NotContains } from 'class-validator';
 import { Role } from '@prisma/client';
-import { ApiExtraModels, ApiProperty } from '@nestjs/swagger';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class SignUpAuthDto extends PartialType(SignInAuthDto) {
+export class SignUpAuthDto {
+    @ApiProperty({example:"example@gmail.com"})
+    @IsNotEmpty()
+    @IsEmail()
+    @NotContains(" ")
+    readonly email: string;
+
+    @ApiProperty({example:"123Password"})
+    @IsNotEmpty()
+    @IsString()
+    @NotContains(" ")
+    @Length(5, 20, { message: 'Password has to be at between 5 and 20 chars'})
+    @Matches(/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#$%&? "])[a-zA-Z0-9!#$%&?]{0,25}$/, 
+    {message: 'The password should contain at least 1 uppercase character, 1 lowercase, 1 number'})
+    readonly password: string;
+    
     @ApiProperty({ enum: ['ADMIN', 'USER']})
     @NotContains(" ")
     @IsNotEmpty()
