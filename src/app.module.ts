87b0c95f2ac32a5934b38607385cbcd6c0849e02
esse <redstrixw@gmail.com>
2024-07-10 13:03:08 +0300

    added multiply env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { MeetupModule } from './meetup/meetup.module';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MeetupModule, AuthModule, DatabaseModule],
+  imports: [
+    MeetupModule,
+    AuthModule,
+    DatabaseModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,16 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { AtStrategy, RtStrategy } from './strategies';
 
 @Module({
-  imports: [JwtModule.register({}), ConfigModule.forRoot({
-    envFilePath: ['.env.development.local', '.env.development'],
-  })],
+  imports: [JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService, AtStrategy, RtStrategy]
+  providers: [AuthService, AtStrategy, RtStrategy],
 })
-
 export class AuthModule {}
