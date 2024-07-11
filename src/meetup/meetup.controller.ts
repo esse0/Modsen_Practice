c30e1c4a423b75c6a1a1d6f21b19f93dbaca6f0e
esse <redstrixw@gmail.com>
2024-07-11 15:53:22 +0300

    fixed swagger schema

--- src/meetup/meetup.controller.ts
+++ src/meetup/meetup.controller.ts
@@ -10,92 +10,231 @@ import {
   UseGuards,
   Query,
   ParseUUIDPipe,
-} from '@nestjs/common';
-import { MeetupService } from './meetup.service';
-import { CreateMeetupDto } from './dto/create-meetup.dto';
-import { UpdateMeetupDto } from './dto/update-meetup.dto';
-import { GetCurrentUser } from 'src/common/decorators';
-import { AtGuard } from 'src/common/guards';
-import { Roles } from 'src/common/decorators/RoleDecorator';
-import { RoleGuard } from 'src/common/guards/role.guard';
-import { ApiResponse, ApiSecurity, ApiTags } from '@nestjs/swagger';
-import { PageOptionsDto } from './dto/page-options.dto';
+} from "@nestjs/common";
+import { MeetupService } from "./meetup.service";
+import { CreateMeetupDto } from "./dto/create-meetup.dto";
+import { UpdateMeetupDto } from "./dto/update-meetup.dto";
+import { GetCurrentUser } from "src/common/decorators";
+import { AtGuard } from "src/common/guards";
+import { Roles } from "src/common/decorators/RoleDecorator";
+import { RoleGuard } from "src/common/guards/role.guard";
+import {
+  ApiResponse,
+  ApiOkResponse,
+  ApiUnauthorizedResponse,
+  ApiBadRequestResponse,
+  ApiInternalServerErrorResponse,
+  ApiSecurity,
+  ApiTags,
+  ApiForbiddenResponse,
+  ApiCreatedResponse,
+} from "@nestjs/swagger";
+import { PageOptionsDto } from "./dto/page-options.dto";
+import { CustomResponseDto } from "src/common/dto/custom-response.dto";
 
-@ApiTags('meetups')
-@Controller('meetups')
+@ApiTags("meetups")
+@Controller("meetups")
 @UseGuards(AtGuard, RoleGuard)
 export class MeetupController {
   constructor(private readonly meetupService: MeetupService) {}
 
-  @ApiSecurity('access-token')
-  @Roles('ADMIN')
+  @ApiCreatedResponse({
+    description: "Successful response",
+    type: CustomResponseDto,
+  })
+  @ApiForbiddenResponse({
+    description: "Access denied",
+    type: CustomResponseDto,
+  })
+  @ApiBadRequestResponse({
+    description: "Bad request",
+    type: CustomResponseDto,
+  })
+  @ApiInternalServerErrorResponse({
+    description: "Internal server error",
+    type: CustomResponseDto,
+  })
+  @ApiUnauthorizedResponse({
+    description: "Unathorized",
+    type: CustomResponseDto,
+  })
+  @ApiSecurity("access-token")
+  @Roles("ADMIN")
   @HttpCode(201)
   @Post()
   @ApiResponse({ status: 201, type: CreateMeetupDto })
   async create(
-    @GetCurrentUser('id') userId: string,
+    @GetCurrentUser("id") userId: string,
     @Body() createMeetupDto: CreateMeetupDto,
   ) {
     return this.meetupService.create(createMeetupDto, userId);
   }
 
-  @ApiSecurity('access-token')
-  @Roles('ADMIN', 'USER')
+  @ApiOkResponse({
+    description: "Successful response",
+    type: CustomResponseDto,
+  })
+  @ApiInternalServerErrorResponse({
+    description: "Internal server error",
+    type: CustomResponseDto,
+  })
+  @ApiUnauthorizedResponse({
+    description: "Unathorized",
+    type: CustomResponseDto,
+  })
+  @ApiSecurity("access-token")
+  @Roles("ADMIN", "USER")
   @HttpCode(200)
   @Get()
   async findAll(@Query() pageOptionsDto: PageOptionsDto) {
     return this.meetupService.findAll(pageOptionsDto);
   }
 
-  @ApiSecurity('access-token')
-  @Roles('ADMIN', 'USER')
+  @ApiOkResponse({
+    description: "Successful response",
+    type: CustomResponseDto,
+  })
+  @ApiBadRequestResponse({
+    description: "Bad request",
+    type: CustomResponseDto,
+  })
+  @ApiInternalServerErrorResponse({
+    description: "Internal server error",
+    type: CustomResponseDto,
+  })
+  @ApiUnauthorizedResponse({
+    description: "Unathorized",
+    type: CustomResponseDto,
+  })
+  @ApiSecurity("access-token")
+  @Roles("ADMIN", "USER")
   @HttpCode(200)
-  @Get(':id')
-  async findOne(@Param('id', ParseUUIDPipe) id: string) {
+  @Get(":id")
+  async findOne(@Param("id", ParseUUIDPipe) id: string) {
     return this.meetupService.findOne(id);
   }
 
-  @ApiSecurity('access-token')
-  @Roles('ADMIN')
+  @ApiOkResponse({
+    description: "Successful response",
+    type: CustomResponseDto,
+  })
+  @ApiForbiddenResponse({
+    description: "Access denied",
+    type: CustomResponseDto,
+  })
+  @ApiBadRequestResponse({
+    description: "Bad request",
+    type: CustomResponseDto,
+  })
+  @ApiInternalServerErrorResponse({
+    description: "Internal server error",
+    type: CustomResponseDto,
+  })
+  @ApiUnauthorizedResponse({
+    description: "Unathorized",
+    type: CustomResponseDto,
+  })
+  @ApiSecurity("access-token")
+  @Roles("ADMIN")
   @HttpCode(200)
-  @Patch(':id')
+  @Patch(":id")
   async update(
-    @GetCurrentUser('id') userId: string,
-    @Param('id', ParseUUIDPipe) id: string,
+    @GetCurrentUser("id") userId: string,
+    @Param("id", ParseUUIDPipe) id: string,
     @Body() updateMeetupDto: UpdateMeetupDto,
   ) {
     return this.meetupService.update(id, userId, updateMeetupDto);
   }
 
-  @ApiSecurity('access-token')
-  @Roles('ADMIN')
+  @ApiOkResponse({
+    description: "Successful response",
+    type: CustomResponseDto,
+  })
+  @ApiForbiddenResponse({
+    description: "Access denied",
+    type: CustomResponseDto,
+  })
+  @ApiBadRequestResponse({
+    description: "Bad request",
+    type: CustomResponseDto,
+  })
+  @ApiInternalServerErrorResponse({
+    description: "Internal server error",
+    type: CustomResponseDto,
+  })
+  @ApiUnauthorizedResponse({
+    description: "Unathorized",
+    type: CustomResponseDto,
+  })
+  @ApiSecurity("access-token")
+  @Roles("ADMIN")
   @HttpCode(200)
-  @Delete(':id')
+  @Delete(":id")
   async remove(
-    @GetCurrentUser('id') userId: string,
-    @Param('id', ParseUUIDPipe) id: string,
+    @GetCurrentUser("id") userId: string,
+    @Param("id", ParseUUIDPipe) id: string,
   ) {
     return this.meetupService.remove(id, userId);
   }
 
-  @ApiSecurity('access-token')
-  @Roles('ADMIN', 'USER')
+  @ApiOkResponse({
+    description: "Successful response",
+    type: CustomResponseDto,
+  })
+  @ApiForbiddenResponse({
+    description: "User not found",
+    type: CustomResponseDto,
+  })
+  @ApiBadRequestResponse({
+    description: "Bad request",
+    type: CustomResponseDto,
+  })
+  @ApiInternalServerErrorResponse({
+    description: "Internal server error",
+    type: CustomResponseDto,
+  })
+  @ApiUnauthorizedResponse({
+    description: "Unathorized",
+    type: CustomResponseDto,
+  })
+  @ApiSecurity("access-token")
+  @Roles("ADMIN", "USER")
   @HttpCode(200)
-  @Post(':id/register')
+  @Post(":id/register")
   async registerUser(
-    @GetCurrentUser('id') userId: string,
-    @Param('id', ParseUUIDPipe) id: string,
+    @GetCurrentUser("id") userId: string,
+    @Param("id", ParseUUIDPipe) id: string,
   ) {
     return this.meetupService.registerUser(id, userId);
   }
 
-  @ApiSecurity('access-token')
-  @Roles('ADMIN', 'USER')
+  @ApiOkResponse({
+    description: "Successful response",
+    type: CustomResponseDto,
+  })
+  @ApiForbiddenResponse({
+    description: "User not found",
+    type: CustomResponseDto,
+  })
+  @ApiBadRequestResponse({
+    description: "Meetup not found",
+    type: CustomResponseDto,
+  })
+  @ApiInternalServerErrorResponse({
+    description: "Internal server error",
+    type: CustomResponseDto,
+  })
+  @ApiUnauthorizedResponse({
+    description: "Unathorized",
+    type: CustomResponseDto,
+  })
+  @ApiSecurity("access-token")
+  @Roles("ADMIN", "USER")
   @HttpCode(200)
-  @Post(':id/unregister')
+  @Post(":id/unregister")
   async unregisterUser(
-    @GetCurrentUser('id') userId: string,
-    @Param('id', ParseUUIDPipe) id: string,
+    @GetCurrentUser("id") userId: string,
+    @Param("id", ParseUUIDPipe) id: string,
   ) {
     return this.meetupService.unregisterUser(id, userId);
   }
--- src/meetup/meetup.service.ts
+++ src/meetup/meetup.service.ts
@@ -2,13 +2,13 @@ import {
   BadRequestException,
   ForbiddenException,
   Injectable,
-} from '@nestjs/common';
-import { CreateMeetupDto } from './dto/create-meetup.dto';
-import { UpdateMeetupDto } from './dto/update-meetup.dto';
-import { DatabaseService } from 'src/database/database.service';
-import { PageOptionsDto } from './dto/page-options.dto';
-import { PageDto } from './dto/page.dto';
-import { PageMetaDto } from './dto/page-meta.dto';
+} from "@nestjs/common";
+import { CreateMeetupDto } from "./dto/create-meetup.dto";
+import { UpdateMeetupDto } from "./dto/update-meetup.dto";
+import { DatabaseService } from "src/database/database.service";
+import { PageOptionsDto } from "./dto/page-options.dto";
+import { PageDto } from "./dto/page.dto";
+import { PageMetaDto } from "./dto/page-meta.dto";
 
 @Injectable()
 export class MeetupService {
@@ -22,7 +22,7 @@ export class MeetupService {
       where: { id: userId },
     });
 
-    if (!user) throw new ForbiddenException('User not found');
+    if (!user) throw new ForbiddenException("User not found");
 
     return this.prismaService.meetup.create({
       data: {
@@ -120,7 +120,7 @@ export class MeetupService {
       },
     });
 
-    if (!meetup) throw new BadRequestException('Meetup not found');
+    if (!meetup) throw new BadRequestException("Meetup not found");
 
     return meetup;
   }
@@ -133,16 +133,16 @@ export class MeetupService {
       where: { id: userId },
     });
 
-    if (!user) throw new ForbiddenException('User not found');
+    if (!user) throw new ForbiddenException("User not found");
 
     const meetup = await this.prismaService.meetup.findUnique({
       where: { id },
     });
 
-    if (!meetup) throw new BadRequestException('Meetup not found');
+    if (!meetup) throw new BadRequestException("Meetup not found");
 
     if (meetup.organizerId != user.id)
-      throw new ForbiddenException('Access denied');
+      throw new ForbiddenException("Access denied");
 
     return this.prismaService.meetup.update({
       where: { id },
@@ -181,16 +181,16 @@ export class MeetupService {
       where: { id: userId },
     });
 
-    if (!user) throw new ForbiddenException('User not found');
+    if (!user) throw new ForbiddenException("User not found");
 
     const meetup = await this.prismaService.meetup.findUnique({
       where: { id: id },
     });
 
-    if (!meetup) throw new BadRequestException('Meetup not found');
+    if (!meetup) throw new BadRequestException("Meetup not found");
 
     if (meetup.organizerId != user.id)
-      throw new ForbiddenException('Access denied');
+      throw new ForbiddenException("Access denied");
 
     return this.prismaService.meetup.delete({
       where: { id },
@@ -211,21 +211,21 @@ export class MeetupService {
       include: { subscribedMeetups: { where: { id } } },
     });
 
-    if (!user) throw new ForbiddenException('User not found');
+    if (!user) throw new ForbiddenException("User not found");
 
     if (user.subscribedMeetups.length != 0)
       throw new BadRequestException(
-        'You are already registered for the meetup',
+        "You are already registered for the meetup",
       );
 
     const meetup = await this.prismaService.meetup.findUnique({
       where: { id },
     });
 
-    if (!meetup) throw new BadRequestException('Meetup not found');
+    if (!meetup) throw new BadRequestException("Meetup not found");
 
     if (meetup.organizerId == userId)
-      throw new BadRequestException('Owner cannot register for the meetup');
+      throw new BadRequestException("Owner cannot register for the meetup");
 
     return this.prismaService.meetup.update({
       where: { id },
@@ -253,10 +253,10 @@ export class MeetupService {
       include: { subscribedMeetups: { where: { id: id } } },
     });
 
-    if (!user) throw new ForbiddenException('User not found');
+    if (!user) throw new ForbiddenException("Meetup not found");
 
     if (user.subscribedMeetups.length == 0)
-      throw new BadRequestException('You are not registered for this meetup');
+      throw new BadRequestException("You are not registered for this meetup");
 
     return this.prismaService.meetup.update({
       where: { id },
